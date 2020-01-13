27aa24a39f787583c200983b650a726d6bfb2e18
Jared Junkin <jared.junkn@duke.edu>
2020-01-13 13:20:36 -0500

    use priority queues to make a method more efficient

--- src/OlympicCandles.java
+++ src/OlympicCandles.java
@@ -1,5 +1,4 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class OlympicCandles{
     public int CountZeros(int[] candles){
@@ -12,17 +11,34 @@ public class OlympicCandles{
         return count;
     }
 
-    public int numberOfNights(int[] candles){
-        int count = 0;
-        int dex = candles.length -1;
-        while(CountZeros(candles) < candles.length - count){
-            count++;
-            dex--;
-            Arrays.sort(candles);
-            for(int j = candles.length - 1; j > dex ; j--){
-                candles[j]--;
-            }
+    public int reverseCompare(int a, int b){
+        //I suppose it's implicit that a is this and b is the second one. 
+        if(a >= b) return -1;
+        return 1;
+    }
+
+    private PriorityQueue<Integer> makeQueue(int[] candles){
+        PriorityQueue<Integer> ret = new PriorityQueue<>(this::reverseCompare);
+        for(int candle : candles){
+            ret.add(candle);
         }
-       return count;
+        return ret;
+    }
+
+    public int numberOfNights(int[] candles){
+        PriorityQueue<Integer> pq = makeQueue(candles);
+        int count = 0;
+        while(true){
+            List<Integer> tempList = new ArrayList<>();
+            for(int k = 0; k <= count; k++){
+                if(pq.size() <= 0) return count;
+                int temp = pq.remove();
+                if(temp == 0) return count;
+                temp--;
+                tempList.add(temp);
+            }
+            pq.addAll(tempList);
+            count++;
+        }
     }
 }
